effac9a65b11c6060cf3421de87bc7fef1dc1c6a
tralph3 <tomasralph2000@gmail.com>
2024-06-22 16:52:50 -0300

    add wall sliding

--- src/physics.c
+++ src/physics.c
@@ -2,10 +2,14 @@
 #include "player.h"
 
 void check_collission(Player *player) {
-    int map_x = player->position.x;
-    int map_y = player->position.y;
-    int tile = world_map[map_x + map_y * MAP_WIDTH];
-    if (tile > 0) {
-        player->position = player->oldPosition;
-    }
+    int prev_map_x = player->oldPosition.x;
+    int prev_map_y = player->oldPosition.y;
+    int curr_map_x = player->position.x;
+    int curr_map_y = player->position.y;
+    int tile_x = world_map[curr_map_x + prev_map_y * MAP_WIDTH];
+    int tile_y = world_map[prev_map_x + curr_map_y * MAP_WIDTH];
+    if (tile_x > 0)
+      player->position.x = player->oldPosition.x;
+    if (tile_y > 0)
+      player->position.y = player->oldPosition.y;
 }
