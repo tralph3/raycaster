44364a6f0373db0d5a3029dc850e0727d1c78f77
tralph3 <tomasralph2000@gmail.com>
2024-07-15 20:05:22 -0300

    cleanup main

--- src/main.c
+++ src/main.c
@@ -1,40 +1,11 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <dirent.h>
-#include "map.h"
 
 #include "game.h"
-#include "textures.h"
-#include "renderer.h"
-
-void generate_map(void) {
-  MapTile data[81] = {
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 0}, (MapTile){2, 0, 3, 1},
-    (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1}, (MapTile){2, 0, 3, 1},
-  };
-  Map map = {
-      .player_start_position = (Vector2){1.5, 1.5},
-      .size = 81,
-      .width = 9,
-      .version = 1,
-      .data = data,
-  };
-  save_map(&map, "./assets/maps/test.map");
-}
 
 int main(void) {
-#if 0
-  generate_map();
-#else
     Game game = create_new_game();
     game_run(&game);
-#endif
     return 0;
 }
